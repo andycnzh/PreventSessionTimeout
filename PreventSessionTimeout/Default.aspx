aa8c74278a459283c31dfb05ef4c4fd9dc5f29d1
AndyZhang1 <witandyzh@gmail.com>
2012-11-20 14:11:16 +0800

    first commit

---
+++ PreventSessionTimeout/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PreventSessionTimeout.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>This page reference this web page:http://www.codeproject.com/Articles/9607/Prevent-Session-Timeout-in-ASP-NET . </p>
+            <p>This code will cause the client to request within 30 seconds of the session timeout the page Reconnect.aspx.</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PreventSessionTimeout/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PreventSessionTimeout
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.AddKeepAlive();
+        }
+
+        private void AddKeepAlive()
+        {
+            int milliSecondsTimeout = (this.Session.Timeout * 60) - 30;
+
+            string script = @"
+<script type='text/javascript'>
+//Number of Reconnects
+var count = 0;
+//Maxinum reconnects setting
+var max = 5;
+
+function Reconnect(){
+
+count++;
+
+if(count < max)
+{
+    window.status = 'Link to Server Refreshed ' + count.toString() + ' time(s)';
+    
+    var img = new Image(1,1);
+
+    img.src= '/Reconnect.aspx';
+}
+
+}
+window.setInterval('Reconnect()', " +
+                                     milliSecondsTimeout.ToString() + @"); //set to length required
+
+</script>
+";
+
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "Refresh", script, false);
+        }
+
+    }
+}
